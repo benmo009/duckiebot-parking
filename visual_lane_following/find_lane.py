114f7cf0d0cf546295beb30df0bc04b5b6b4ad62
Ben <benmo009@gmail.com>
2020-11-01 16:35:38 -0500

    Added HSV filter to find lane lines

--- visual_lane_following/find_lane.py
+++ visual_lane_following/find_lane.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load the image
-img = cv2.imread('practice_image_3.png')
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.show()
+img = cv2.imread('practice_image_2.png')
+# plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+# plt.show()
 
 # Cut the image
 img = img[120::, :]
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.show()
+# plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+# plt.show()
 
 # Warp the image to get an aerial view
 image_H = (img.shape)[0]
@@ -23,11 +23,32 @@ dst = np.float32([[new_Left,image_H], [new_Right, image_H], [0,0], [image_W, 0]]
 M = cv2.getPerspectiveTransform(src, dst)
 Minv = cv2.getPerspectiveTransform(dst, src)  # Use to unwarp the image
 
-warped_img = cv2.warpPerspective(img, M, (image_W, image_H))
-plt.imshow(cv2.cvtColor(warped_img, cv2.COLOR_BGR2RGB))
-plt.show()
+img_warped = cv2.warpPerspective(img, M, (image_W, image_H))
+# plt.imshow(cv2.cvtColor(img_warped, cv2.COLOR_BGR2RGB))
+# plt.show()
 
 # Find bounds on the lanes
-gray = cv2.cvtColor(warped_img, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Grayscale Image", gray)
+
+# Transform image to HSV color space
+img_hsv = cv2.cvtColor(img_warped, cv2.COLOR_BGR2HSV)
+# plt.imshow(img_hsv)
+# plt.show()
+
+# Filter out everything but yellow (dotted lines) and white (side lines)
+yellow_lower = np.array([20, 75, 100])
+yellow_upper = np.array([30, 255, 255])
+white_lower = np.array([0,0,1])
+white_upper = np.array([120,5,255])
+
+img_yellow = cv2.inRange(img_hsv, yellow_lower, yellow_upper)
+img_white = cv2.inRange(img_hsv, white_lower, white_upper)
+combined_filtered = img_yellow + img_white
+
+# Show all of the images
+cv2.imshow('Aerial View', img_warped)
+cv2.imshow('HSV Image', img_hsv)
+cv2.imshow('Combined Mask', combined_filtered)
+
+cv2.imshow('Filtered - Combined', cv2.bitwise_and(img_warped, img_warped, mask=combined_filtered))
+
 cv2.waitKey(0)
